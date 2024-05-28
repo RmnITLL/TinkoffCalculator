8a9ec1d4bfa7695efa749594e4624a3a6c500e8b
Roman <inetmilo@rambler.ru>
2024-05-28 12:32:14 +0300

    что-то 2

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -164,4 +164,16 @@ class ViewController: UIViewController {
         }
     }
 
+/*
+ 
+ ! [rejected]        lesson11 -> lesson11 (non-fast-forward)
+ error: failed to push some refs to 'github.com:RmnITLL/TinkoffCalculator.git'
+ hint: Updates were rejected because the tip of your current branch is behind
+ hint: its remote counterpart. Integrate the remote changes (e.g.
+ hint: 'git pull ...') before pushing again.
+ hint: See the 'Note about fast-forwards' in 'git push --help' for details.
+ 
+ Что надо сделать, чтобы исправить ошибку?
+ 
+ */
 
